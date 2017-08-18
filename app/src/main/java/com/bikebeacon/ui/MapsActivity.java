6a5eb5917b3687ed2faea70e2b03d11bb92b1bef
AlonGoltzman <alon.goltzman@gmail.com>
2017-08-18 21:55:18 +0300

    Commit 1.3.1

--- app/src/main/java/com/bikebeacon/ui/MapsActivity.java
+++ app/src/main/java/com/bikebeacon/ui/MapsActivity.java
@@ -1,4 +1,4 @@
-package com.bikebeacon;
+package com.bikebeacon.ui;
 
 import android.Manifest;
 import android.content.Intent;
--- app/src/main/java/com/bikebeacon/ui/SettingsActivity.java
+++ app/src/main/java/com/bikebeacon/ui/SettingsActivity.java
@@ -1,4 +1,4 @@
-package com.bikebeacon;
+package com.bikebeacon.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
