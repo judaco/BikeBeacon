ebb7b789d227e3b5e734bb4ea1104e56444fe729
Judaco <judacossa@gmail.com>
2017-08-18 21:40:57 +0300

    Commit 1.2.1
    Updated dirs

--- app/src/androidTest/java/com/bikebeacon/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/bikebeacon/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.bikebeacon;
+package com.example.juda.bbexample;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -7,7 +7,7 @@ import android.support.test.runner.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.bikebeacon", appContext.getPackageName());
+        assertEquals("com.example.juda.bbexample", appContext.getPackageName());
     }
 }
